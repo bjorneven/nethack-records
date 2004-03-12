49e64222c33b8ac21d41dc98ebf2e3c2b629bf11
bjorn <bjorn@26df0369-99d4-0310-a9ef-c12d98c1c1e3>
2004-03-12 00:00:43 +0000

    Script now includes directory of new css-files

--- trunk/create-distribution.sh
+++ trunk/create-distribution.sh
@@ -6,10 +6,12 @@ BASEDIR=dist/Nethack-Records-$VERSION
 mkdir -p $BASEDIR/lib/HTML
 mkdir -p $BASEDIR/docs
 mkdir -p $BASEDIR/tmpl
+mkdir -p $BASEDIR/css
 cp -a CREDITS LICENSE CHANGELOG INSTALL README nethack.css records.cgi *.png $BASEDIR
+cp -a css/*.css $BASEDIR/css
 cp -a lib/HTML/Template.pm lib/HTML/ARTISTIC lib/HTML/GPL lib/HTML/ANNOUNCE lib/HTML/FAQ lib/HTML/README $BASEDIR/lib/HTML
 cp -a lib/optionsParser.pm $BASEDIR/lib
-cp -a docs/* $BASEDIR//docs
+cp -a docs/* $BASEDIR/docs
 cp -a tmpl/* $BASEDIR/tmpl
 #cp -a *.tmpl dist/Nethack-Records/
 cp -a options.cfg $BASEDIR/

24d2225a18c5852a3990d8066276b0e3dd315c84
bjorn <bjorn@26df0369-99d4-0310-a9ef-c12d98c1c1e3>
2007-06-06 00:28:23 +0000

    reflects changes in dir structure

--- trunk/create-distribution.sh
+++ trunk/create-distribution.sh
@@ -7,14 +7,16 @@ mkdir -p $BASEDIR/lib/HTML
 mkdir -p $BASEDIR/docs
 mkdir -p $BASEDIR/tmpl
 mkdir -p $BASEDIR/css
-cp -a CREDITS LICENSE UPGRADE CHANGELOG INSTALL README index.cgi *.png $BASEDIR
+mkdir -p $BASEDIR/img
+cp -a CREDITS LICENSE UPGRADE CHANGELOG INSTALL README index.cgi $BASEDIR
 cp -a css/*.css $BASEDIR/css
 cp -a lib/HTML/Template.pm lib/HTML/ARTISTIC lib/HTML/GPL lib/HTML/ANNOUNCE lib/HTML/FAQ lib/HTML/README $BASEDIR/lib/HTML
 cp -a lib/optionsParser.pm $BASEDIR/lib
 cp -a docs/* $BASEDIR/docs
+cp -a img/* $BASEDIR/img
 cp -a tmpl/* $BASEDIR/tmpl
 #cp -a *.tmpl dist/Nethack-Records/
-cp -a options.cfg $BASEDIR/
+cp -a options.cfg-default $BASEDIR/
 cd dist
 tar czf ../Nethack-Records-$VERSION.tar.gz Nethack-Records-$VERSION
 cd ..
